5ecdcf5bb3f8cadc00a4fb472f0a90ac2e697899
ricardvs <riaraos@gmail.com>
2018-05-28 22:19:03 -0400

    Nuevas modificaciones

--- main/destructuring.js
+++ main/destructuring.js
@@ -2,11 +2,19 @@ function foo() {
     return [1, 2, 3];
 }
 
+function anotherFoo()
+{
+    return { 'name': 'Zazú', 'lastName': 'Araos', 'title': 'Chanchulina'};
+}
+
 function destructuringExample() {
     let tmp = foo(),
         x = tmp[0], y = tmp[1], z = tmp[2];
 
     console.log(x, y, z);
+
+    var {'name': nombre, 'lastName': apellido, 'title': titulo} = anotherFoo();
+    console.log(nombre, apellido, titulo);
 
     return {x:x, y:y, z:z};
 }
